d062cae9cd8a927d728c48cb135624c785d4c772
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2025-02-25 12:21:44 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static("public"));
+const io = new Server(server);
+
+//socket.io
+io.on("connection", (socket) => {
+  socket.on("user-message", (message) => {
+    io.emit("message", message);
+  });
+});
+
+app.get("/", (req, res) => {
+  return res.sendFile("/public/index.html");
+});
+
+server.listen(9000, () => {
+  console.log("server started at port number : 9000");
+});
